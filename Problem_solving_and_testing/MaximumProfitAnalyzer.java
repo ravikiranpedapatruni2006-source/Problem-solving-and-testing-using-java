5c33e7c611a3a34a09f3d6a4b98080e2690429a6
Ravi Kiran <ravikiranpedapatrani2006@gmail.com>
2026-08-12 21:01:11 +0530

    Add MaximumProfitAnalyzer.java

---
+++ Problem_solving_and_testing/MaximumProfitAnalyzer.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MaximumProfitAnalyzer {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+
+            currentSum += value;
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        System.out.println(maxSum);
+
+        sc.close();
+    }
+}
