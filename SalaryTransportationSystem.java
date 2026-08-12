374a2e6340d640e11bb26ddd7736f6aee85f5dd4
Ravi Kiran <ravikiranpedapatrani2006@gmail.com>
2026-08-12 11:32:26 +0530

    Add Salary Transportation System

---
+++ SalaryTransportationSystem.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class SalaryTransportationSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> salaries = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            salaries.add(sc.nextInt());
+        }
+        salaries.stream()
+                .map(salary -> salary + salary / 10)
+                .forEach(salary -> System.out.print(salary + " "));
+    }
+}
